f4536de3ff9f2e9c43a8936f875398f6237bf34e
Christian Westhoff <westhoffchristian1@gmail.com>
2021-01-26 12:37:08 +0100

    Add console startup

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,15 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import { greeter } from "./lib/counter";
 
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+process.on('unhandledRejection', (err) => {
+  console.error(err instanceof Error ? err.message : err);
+  process.exit(-1);
+});
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing missing return type definitions for greeter function
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
-}
+(async () => {
+  try {
+      console.log(await greeter("World"));
+      process.exit(0);
+  } catch(e) {
+      throw e;
+  }
+})();
